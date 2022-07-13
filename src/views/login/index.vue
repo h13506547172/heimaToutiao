6086829d4b21d771913a87b7c588593cb2f1eab3
tukasa <497116162@qq.com>
2022-07-13 17:29:44 +0800

    完成了login页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,25 @@
 <template>
   <!-- 登录页面 -->
   <div class="login-container">
-    <van-nav-bar title="登录" />
-    <van-form @submit="onSubmit">
+    <van-nav-bar title="登录" @click-left="$router.back()">
+      <template #left>
+        <van-icon name="cross" color="#fff" />
+      </template>
+    </van-nav-bar>
+    <van-form @submit="onSubmit" ref="loginForm">
       <van-field
-        v-model="user.phoneNum"
+        v-model="user.mobile"
         type="number"
+        maxlength="11"
         name="phoneNum"
         placeholder="请输入11位手机号"
-        :rules="[{ required: true, message: '请填写用户名' }]"
+        :rules="[
+          { required: true, message: '请填写手机号' },
+          {
+            pattern: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/,
+            message: '请填写正确的手机号'
+          }
+        ]"
       >
         <template #left-icon>
           <i class="iconfont icon-shouji"></i>
@@ -17,16 +28,35 @@
       <van-field
         v-model="user.code"
         type="number"
+        maxlength="6"
         name="phoneNum"
         placeholder="请输入验证码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        :rules="[
+          { required: true, message: '请填写验证码' },
+          {
+            pattern: /^\d{6}$/,
+            message: '请填写正确的验证码'
+          }
+        ]"
       >
         <template #left-icon>
           <i class="iconfont icon-yanzhengma"></i>
         </template>
         <!-- 验证码按钮 -->
         <template #button>
-          <van-button round class="codeBtn">发送验证码</van-button>
+          <van-count-down
+            :time="6 * 1000"
+            v-if="countDownShow"
+            @finish="countDownShow = false"
+          />
+          <van-button
+            v-else
+            native-type="button"
+            round
+            class="codeBtn"
+            @click="onSendSms"
+            >发送验证码</van-button
+          >
         </template>
       </van-field>
       <div style="margin: 16px">
@@ -37,12 +67,51 @@
 </template>
 
 <script>
+import { loginAPI, sendSmsAPI } from '@/api/index'
 export default {
   data() {
     return {
+      countDownShow: false,
+      // 手机输入框内容和验证码内容
       user: {
-        phoneNum: '',
+        mobile: '',
         code: ''
+      }
+    }
+  },
+  methods: {
+    // 点击提交,满足规则触发提交事件
+    async onSubmit() {
+      try {
+        const res = await loginAPI(this.user)
+        // console.log(res)
+        // 将token存入vuex中并存入本地
+        this.$store.commit('setUserToken', res.data.data)
+        this.$router.back()
+      } catch (err) {
+        // console.log(err)
+        if (err.response && err.response.status === 400) {
+          this.$toast.fail('手机号或者验证码错误')
+        } else {
+          this.$toast.fail('登录失败，请稍微再试')
+        }
+      }
+    },
+    // 发送验证码事件
+    async onSendSms() {
+      // 验证手机号是否正确
+      this.$refs.loginForm.validate('phoneNum')
+      // 让倒计时显现
+      this.countDownShow = true
+      // 发送验证码请求
+      try {
+        const res = await sendSmsAPI(this.user.mobile)
+        // console.log(res)
+        if (res.status === 200) {
+          this.$toast.success('短信验证码发送成功')
+        }
+      } catch (err) {
+        this.$toast.fail('发送失败，请稍微再试')
       }
     }
   }
@@ -73,7 +142,7 @@ export default {
       height: 46px;
       background-color: #ededed;
       border-radius: 23px;
-      font-size:22px;
+      font-size: 22px;
       padding: 0;
       color: #666666;
     }
